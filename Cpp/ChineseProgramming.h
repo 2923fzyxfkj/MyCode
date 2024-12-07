660467f08da13a16e707c5a38b15f0f6e0e55462
2923fzyxfkj <162860026+2923fzyxfkj@users.noreply.github.com>
2024-12-07 15:43:49 +0800

    Create ChineseProgramming.h
    C++的中文编程头文件

---
+++ Cpp/ChineseProgramming.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#define 打印iostream cout
+#define 打印stdio printf
+#define 传参 <<
+#define 返回 return
+#define 一 1
+#define 二 2
+#define 三 3
+#define 四 4
+#define 五 5
+#define 六 6
+#define 七 7
+#define 八 8
+#define 九 9
+#define 零 0
+#define 定义 int
+#define 主函数 main()
+#define 开始 {
+#define 结束 }
+#define 语句结束 ;
+/* // Test Author:Bilibili:F_are_loss Gitcode:a11451419192929 Github:2923fzyxfkj
+定义 主函数 开始
+	打印iostream 传参 "Hello, World!" 语句结束
+结束
+*/
+#define 使用 using
+#define 名字空间 namespace
+// 作者不知到std是什么所以没写
+#define 加 +
+#define 减 -
+#define 乘 *
+#define 除 /
+#define 和 &&
+#define 或 ||
+#define 有限循环 for
+#define 无线或有限循环 while
+#define 结束换行 endl
+#define 定义函数 void
+#define 公共_函数 public:
+#define 私有_函数 private:
+#define 做 do
+#define 检测大小_内存 sizeof
+#define 介绍_字符串 "本产品由Bilibili:F_are_loss,Gitcode:a11451419192929,Github:2923fzyxfkj制作,已开源,编辑器:VS2022PREVIEM(本来想用CLion的但是他写那个*CMake亥死我了,我再写这个帮助的时候C盘只剩1个GB了,这VS内存占我22个GB,服了,会更新的)		English:This product is produced by Bilibili:F_are_Loss, Gitcode: a11451419192929, making: 2923 fzyxfkj production, open source, editor: VS2022PREVIEM (originally wanted to use the CLion but he write the * CMake hai the shit out of me, when I write this help again disk C with only 1 GB, it VS memory accounts for 22 GB, take it, will update)"
+// 自动输出帮助菜单
+使用 名字空间 std;
+定义 introduce() 开始
+打印iostream 传参 介绍_字符串 传参 结束换行 语句结束
+返回 零;
+结束
